e88389ca528a2c6585972d3e4db2f8d9dcb19a51
LeticiaGrein <leticiagreinschool@gmail.com>
2024-10-04 11:38:46 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <!--NÃO ALTERAR-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Produtos</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="src/css/styles.css">
+</head>
+
+<body>
+    <!--NÃO ALTERAR-->
+    <div class="container-fluid">
+        <div class="row">
+            <nav class="col-md-2 d-none d-md-block bg-purple sidebar">
+                <div class="logo">
+                    <i class="bi bi-0-circle-fill"></i>
+                    <h2>HORIZON+</h2>
+                </div>
+                <div class="sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">
+                                <i class="bi bi-1-circle-fill"></i>
+                                <i class="bi bi-phone-fill icon-large"></i>
+                                <span>Produtos</span>
+                                <i id="indicator" class="bi bi-caret-left-fill indicator"></i>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-basket2-fill icon-large"></i>
+                                <span>Pedidos</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-people-fill icon-large"></i>
+                                <span>Fornecedores</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-person-badge-fill icon-large"></i>
+                                <span>Funcionários</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-box-fill icon-large"></i>
+                                <span>Estoque</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                                <i class="bi bi-pc-display-horizontal icon-large"></i>
+                                <span>Equipamentos</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="user">
+                        <div class="nav-item">
+                            <div class="d-flex align-items-center card-user">
+                                <i class="bi bi-person-circle users"></i>
+                                <div class="ml-2">
+                                    <div>Funcionário</div>
+                                    <div class="text-muted" style="font-size: 0.8em;">Função xyz</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <!--NÃO ALTERAR-->
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+                    <h1 class="h2">TÍTULO DA PÁGINA</h1>
+                </div>
+                <!--NÃO ALTERAR-->
+                <!--MEXER AQUI -->
+                <form>
+  <div class="mb-3">
+    <label for="exampleInputName" class="form-label">Nome Completo</label>
+    <input type="text" class="form-control" id="nome">
+  </div>
+  <select class="form-select" aria-label="cargo">
+  <option selected>Selecione seu cargo:</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+<select class="form-select" aria-label="setor">
+  <option selected>Selecione seu setor:</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+<div class="mb-3">
+    <label for="exampleInputPhone" class="form-label">Telefone:</label>
+    <input type="text" class="form-control" id="telefone">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email:</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Nós nunca iremos compartilhar seu email com alguém.</div>
+  </div>
+   
+    <div class="mb-3">
+    <label for="exampleInputNascimento" class="form-label">Data de Nascimento:</label>
+    <input type="date" class="form-control" id="nascimento">
+</div>
+<br>
+    <div class="input-group mb-3">
+  <span class="input-group-text">Salário:</span>
+  <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+    </div>
+    <select class="form-select" aria-label="setor">
+  <option selected>Selecione o método de pagamento:</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+    <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+                <!--MEXER AQUI -->
+                <!--NÃO ALTERAR-->
+            </main>
+        </div>
+    </div>
+    <!--NÃO ALTERAR-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="src/js/script.js"></script>
+    <!--NÃO ALTERAR-->
+</body>
+
+</html>
