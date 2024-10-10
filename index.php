d50e27d5812b0a56ec060772772e57208eef3858
LeticiaGrein <leticiagreinschool@gmail.com>
2024-10-10 08:34:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,9 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Produtos</title>
     <link rel="stylesheet" href="src/css/styles.css">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     
 </head>
 
@@ -78,15 +76,18 @@
 
             <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
-                    <h1 class="h2">TÍTULO DA PÁGINA</h1>
+                    <h1 class="h2">CADASTRO DE FUNCIONARIOS</h1>
                 </div>
                 <!--NÃO ALTERAR-->
                 <!--MEXER AQUI -->
+                <div class="conteiner"></div>
                 <form class="" method="" action="">
                     <div class="mb-3">
                         <label for="exampleInputName" class="form-label">Nome Completo</label>
                         <input type="text" class="form-control" id="nome">
                     </div>
+                <div class="row col-md">
+
                     <select class="form-select" aria-label="cargo">
                         <option selected>Selecione seu cargo:</option>
                         <option value="1">One</option>
@@ -99,9 +100,10 @@
                         <option value="2">Two</option>
                         <option value="3">Three</option>
                     </select>
+                </div>
                     <div class="mb-3">
                         <label for="exampleInputPhone" class="form-label">Telefone:</label>
-                        <input type="text" class="form-control" id="telefone">
+                        <input  type="text" class="phone form-control" id="telefone" maxlength="15" onkeyup="handlephone(event)" >
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">Email:</label>
@@ -116,17 +118,20 @@
                     <br>
                     <div class="input-group mb-3">
                         <span class="input-group-text">Salário:</span>
-                        <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+                        <input type="text" class="salary form-control"id="salary" aria-label="Amount (to the nearest dollar)" maxlength="15" onkeyup="handlesalary(event)">
                     </div>
+                    <div class="payment">
                     <select class="form-select" aria-label="setor">
                         <option selected>Selecione o método de pagamento:</option>
                         <option value="1">One</option>
                         <option value="2">Two</option>
                         <option value="3">Three</option>
                     </select>
+                    </div>
                     <br>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
+                
 
 
                 <!--MEXER AQUI -->
@@ -135,10 +140,13 @@
         </div>
     </div>
     <!--NÃO ALTERAR-->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="src/js/script.js"></script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    <script type='text/javascript' src='//code.jquery.com/jquery-compat-git.js'></script>
+    <script type='text/javascript' src='//igorescobar.github.io/jQuery-Mask-Plugin/js/jquery.mask.min.js'></script>
+    <script src="script.js"></script>
     <!--NÃO ALTERAR-->
 </body>
 
